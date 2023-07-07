8a222db0636c78c16291ed23154bd5372b5f0a86
webshenzhengyi <15713571587@163.com>
2023-07-07 09:16:29 +0800

    更新时间

---
+++ .vuepress/config.js
@@ -0,0 +1,26 @@
+const moment = require('moment');
+moment.locale("zh-cn")
+module.exports = {
+  title: "Vuepress",
+  themeConfig: {
+    logo: "/assets/img/hero.png",
+    lastUpdated: '更新时间', // string | boolean
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about/' },
+      { text: 'External', link: 'https://google.com' },
+    ]
+  },
+  plugins: [
+    [
+      '@vuepress/last-updated',
+      {
+        transformer: (timestamp) => {
+          // 不要忘了安装 moment
+          const moment = require('moment')
+          return moment(timestamp).format("LLLL")
+        }
+      }
+    ]
+  ]
+};
